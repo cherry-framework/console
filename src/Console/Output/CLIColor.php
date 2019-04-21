44db0a0db8098bceb962d929071a1259c5a6f742
ABGEO <takalandzet@gmail.com>
2019-04-21 19:07:01 +0400

    Use color constants for output color definition.

--- src/Console/Output/CLIColor.php
+++ src/Console/Output/CLIColor.php
@@ -24,40 +24,33 @@ namespace Cherry\Console\Output;
  */
 class CLIColor
 {
-    private $_background_colors = array();
-    private $_foreground_colors = array();
+    // Define foreground color constants
+    const F_BLACK        = '0;30';
+    const F_WHITE        = '1;37';
+    const F_DARK_GRAY    = '1;30';
+    const F_LIGHT_GRAY   = '0;37';
+    const F_BLUE         = '0;34';
+    const F_LIGHT_BLUE   = '1;34';
+    const F_GREEN        = '0;32';
+    const F_LIGHT_GREEN  = '1;32';
+    const F_CYAN         = '0;36';
+    const F_LIGHT_CYAN   = '1;36';
+    const F_RED          = '0;31';
+    const f_LIGHT_RED    = '1;31';
+    const F_PURPLE       = '0;35';
+    const F_LIGHT_PURPLE = '1;35';
+    const F_BROWN        = '0;33';
+    const F_YELLOW       = '1;33';
 
-    /**
-     * CLIColor constructor.
-     */
-    public function __construct()
-    {
-        $this->_background_colors['black'] = '40';
-        $this->_background_colors['red'] = '41';
-        $this->_background_colors['green'] = '42';
-        $this->_background_colors['yellow'] = '43';
-        $this->_background_colors['blue'] = '44';
-        $this->_background_colors['magenta'] = '45';
-        $this->_background_colors['cyan'] = '46';
-        $this->_background_colors['light_gray'] = '47';
-
-        $this->_foreground_colors['black'] = '0;30';
-        $this->_foreground_colors['dark_gray'] = '1;30';
-        $this->_foreground_colors['blue'] = '0;34';
-        $this->_foreground_colors['light_blue'] = '1;34';
-        $this->_foreground_colors['green'] = '0;32';
-        $this->_foreground_colors['light_green'] = '1;32';
-        $this->_foreground_colors['cyan'] = '0;36';
-        $this->_foreground_colors['light_cyan'] = '1;36';
-        $this->_foreground_colors['red'] = '0;31';
-        $this->_foreground_colors['light_red'] = '1;31';
-        $this->_foreground_colors['purple'] = '0;35';
-        $this->_foreground_colors['light_purple'] = '1;35';
-        $this->_foreground_colors['brown'] = '0;33';
-        $this->_foreground_colors['yellow'] = '1;33';
-        $this->_foreground_colors['light_gray'] = '0;37';
-        $this->_foreground_colors['white'] = '1;37';
-    }
+    // Define background color constants
+    const B_BLACK        = '40';
+    const B_RED          = '41';
+    const B_GREEN        = '42';
+    const B_YELLOW       = '43';
+    const B_BLUE         = '44';
+    const B_MAGENTA      = '45';
+    const B_CYAN         = '46';
+    const B_LIGHT_GRAY   = '47';
 
     /**
      * Colorize string.
@@ -68,22 +61,16 @@ class CLIColor
      *
      * @return string Colorized text.
      */
-    public function getColoredString($string, $background_color = null,
-        $foreground_color = null
+    public function getColoredString($string, $background_color = '',
+        $foreground_color = ''
     ) {
         $colored_string = "";
 
-        // Check if given background color found
-        if (isset($this->_background_colors[$background_color])) {
-            $colored_string .= "\033[" .
-                $this->_background_colors[$background_color] . "m";
-        }
+        // Add background color
+        $colored_string .= "\033[" . $background_color . "m";
 
-        // Check if given foreground color found
-        if (isset($this->_foreground_colors[$foreground_color])) {
-            $colored_string .= "\033[" .
-                $this->_foreground_colors[$foreground_color] . "m";
-        }
+        // Add foreground color
+        $colored_string .= "\033[" . $foreground_color . "m";
 
         // Add string and end coloring
         $colored_string .= $string . "\033[0m";
