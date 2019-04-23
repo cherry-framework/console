530ba2462d48a2e25f2b1e92450479fcffe796f5
ABGEO <takalandzet@gmail.com>
2019-04-23 20:48:40 +0400

    Validate Server on PHP_CodeSniffer.

--- src/Console/Command/Server.php
+++ src/Console/Command/Server.php
@@ -1,17 +1,38 @@
 <?php
+/**
+ * The file contains Server trait
+ *
+ * PHP version 5
+ *
+ * @category Library
+ * @package  Cherry
+ * @author   Temuri Takalandze <takalandzet@gmail.com>
+ * @license  https://github.com/cherry-framework/console/blob/master/LICENSE MIT
+ * @link     https://github.com/cherry-framework/console
+ */
 
 namespace Cherry\Console\Command;
 
 use Cherry\Console\Input\ArgvInput;
 use Cherry\Console\Output\Output;
 
+/**
+ * Server Trait for Cherry Console.
+ *
+ * @category Library
+ * @package  Cherry
+ * @author   Temuri Takalandze <takalandzet@gmail.com>
+ * @license  https://github.com/cherry-framework/console/blob/master/LICENSE MIT
+ * @link     https://github.com/cherry-framework/console
+ */
 trait Server
 {
     /**
      * Run PHP Development server from CLI
      *
-     * @param ArgvInput $input
-     * @param Output $output
+     * @param ArgvInput $input  CLI Input interface
+     * @param Output    $output CLI Output interface
+     *
      * @return void
      */
     private function _server(ArgvInput $input, Output $output)
